f6984082629624956fde6596cacdfb51d366eef2
Ashwin Chatterji <ashwinath@hotmail.com>
2023-11-02 21:28:43 +0800

    Skip if empty line, format to use telegram language

--- process.py
+++ process.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 def main():
     data = {}
@@ -7,20 +9,24 @@ def main():
         for index, row in enumerate(reader):
             if index == 0:
                 continue
+            if not row:
+                continue
             dsplit = row[0].split("/")
-            date = f"{dsplit[2]}-{dsplit[1]}-01"
+            d = f"{dsplit[2]}-{dsplit[1]}-01"
             amount_str = row[2]
             if not amount_str:
                 continue
 
             amount = float(amount_str.replace(",", ""))
-            if date in data:
-                data[date] += amount
+            if d in data:
+                data[d] += amount
             else:
-                data[date] = amount
+                data[d] = amount
     for key, value in reversed(data.items()):
         if value:
-            print(f"{key},Credit Card,{value:.2f}")
+            year, month, day = [int(i) for i in key.split("-")]
+            end_of_month = date(year, month, day) + relativedelta(months=1) - relativedelta(days=1)
+            print(f"ADD CC {value:.2f} {end_of_month}")
 
 
 if __name__ == "__main__":
